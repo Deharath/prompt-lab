1b789723052469466de67de87efaa662a1fb4cac
Deharath <gracjando@gmail.com>
2025-06-25 23:42:09 +0200

    chore: commit all outstanding changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: [
       './tsconfig.json',
@@ -8,4 +9,56 @@ module.exports = {
     ],
     tsconfigRootDir: __dirname,
   },
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+      },
+    ],
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '.eslintrc.js',
+  ],
+  overrides: [
+    {
+      files: [
+        'vitest.config.mts',
+        '**/vite.config.ts',
+        '**/vitest.config.ts',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+      ],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
 };
