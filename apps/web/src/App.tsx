60da206042d0b93854aca43dff0b065391e494f2
Gracjan <gracjando@gmail.com>
2025-06-30 22:43:43 +0200

    feat: add diff viewer page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home.js';
-import DiffPage from './DiffPage.js';
+import DiffPage from './pages/DiffPage.js';
 
 const App = () => (
   <BrowserRouter>
